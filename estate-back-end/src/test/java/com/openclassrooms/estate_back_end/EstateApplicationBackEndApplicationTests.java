09049c26ecf76ab5b3c823ec9164437cab57bddb
Erika Belicova <146355532+Erika-Belicova@users.noreply.github.com>
2025-03-03 00:18:20 +0100

    Reorganize project structure to separate front-end and back-end, generate and add estate-back-end with Spring Initializr

---
+++ estate-back-end/src/test/java/com/openclassrooms/estate_back_end/EstateApplicationBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.estate_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstateApplicationBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
