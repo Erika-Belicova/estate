5e77027b5b4f931f10e3f476dbc3eaa744276ebe
Erika Belicova <146355532+Erika-Belicova@users.noreply.github.com>
2025-03-04 06:50:25 +0100

    Create User, Rental, and Message entities to connect to the database

---
+++ estate-back-end/src/main/java/com/openclassrooms/estate_back_end/model/Rental.java
@@ -0,0 +1,121 @@
+package com.openclassrooms.estate_back_end.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "RENTALS")
+public class Rental {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer rentalId;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "surface")
+	private BigDecimal surface;
+	
+	@Column(name = "price")
+	private BigDecimal price;
+
+	@Column(name = "picture")
+	private String picture;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@ManyToOne
+    @JoinColumn(name = "owner_id", referencedColumnName = "id", nullable = false)
+    private User owner;
+	
+	@Column(name = "created_at", updatable = false)
+	private LocalDateTime createdAt;
+
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+
+	public Integer getRentalId() {
+		return rentalId;
+	}
+
+	public void setRentalId(Integer rentalId) {
+		this.rentalId = rentalId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getSurface() {
+		return surface;
+	}
+
+	public void setSurface(BigDecimal surface) {
+		this.surface = surface;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public String getPicture() {
+		return picture;
+	}
+
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public User getOwner() {
+		return owner;
+	}
+
+	public void setOwner(User owner) {
+		this.owner = owner;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	 
+}
